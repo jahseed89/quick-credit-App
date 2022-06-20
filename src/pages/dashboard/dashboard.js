a73e967ab56fb8ad242c5c0b982c07abfa59ea3e
jahseed89 <samson.ocran@gmail.com>
2022-06-20 15:19:12 +0100

    first commit

---
+++ src/pages/dashboard/dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./dashboard.scss";
+
+
+const Dashboard = ()=>{
+    return (
+        <div className="container">
+            <h1>Welcome to my React Page</h1>
+        </div>
+    )
+}
+
+export default Dashboard;
